82c90f62ab49f592bccc12576c5da5b5b0d18096
Riley Morris <rileymichaelmorris@gmail.com>
2022-11-17 17:38:54 -0500

    fix box sizing to always be square

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function genGrid(size){
     const mainBox = document.querySelector('#mainBox');
+    const boxLength = ((1/Math.sqrt(size)) * 100).toString() + '%'
     for (let i = 0; i <= Math.sqrt(size); i++){
          const rowBox = document.createElement('div');
          rowBox.classList.add('rowBox')
+         rowBox.style.height = boxLength
+         rowBox.style.width = '100%'
          mainBox.appendChild(rowBox)
-         console.log(i)
+         
          for (let i=0; i<=Math.sqrt(size); i++){
-            console.log(i + 10)
             const innerBox = document.createElement('div');
             innerBox.classList.add('innerBox');
+            innerBox.style.height = '100%';
+            innerBox.style.width = boxLength;
+            console.log(boxLength)
             rowBox.appendChild(innerBox);
          }
     }
